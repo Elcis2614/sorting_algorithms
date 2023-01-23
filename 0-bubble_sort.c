2c2de09441ec711a2908e32ad7abf7c480bf3118
Cishugi Busole Elisée <elisereflorbusole07@gmail.com>
2023-01-23 09:21:58 -0800

    beautiful bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	if (array != NULL && size > 2)
+	if (array != NULL && size >= 2)
 		for (i = 0; i < size - 1; i++)
 		{
 			for (j = 0; j < size - i - 1; j++)
